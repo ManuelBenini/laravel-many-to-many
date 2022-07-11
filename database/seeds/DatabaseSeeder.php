1bd76121a5c03e87bdda005a2cbf73b95b9ff90d
ManuelBenini <manuelbenini1905@gmail.com>
2022-07-11 13:17:32 +0200

    Creati model/migration/seeder dei Tags

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PostsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(UpdatePostsSeeder::class);
+        $this->call(TagsTableSeeder::class);
     }
 }

c44b5e0505f91ece2edba75ad02484b6888fcb5f
ManuelBenini <manuelbenini1905@gmail.com>
2022-07-11 14:12:18 +0200

    Aggiunte relazioni belongsToMany ai model Post/Tag

--- app/Post.php
+++ app/Post.php
@@ -11,6 +11,10 @@ class Post extends Model
         return $this->belongsTo('App\Category');
     }
 
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
+    }
+
     protected $fillable = [
         'title',
         'slug',

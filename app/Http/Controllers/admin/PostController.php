4288c10607e8c3dfb6426d6203a33e2878f7889a
ManuelBenini <manuelbenini1905@gmail.com>
2022-07-11 13:11:25 +0200

    Primo commit

---
+++ app/Http/Controllers/admin/PostController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('id', 'DESC')->paginate(10);
+        $categories = Category::all();
+        return view('admin.posts.index', compact('posts', 'categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.posts.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostRequest $request)
+    {
+        $data = $request->all();
+        $data['slug'] = Post::generateSlug($data['title']);
+
+        $new_post = new Post();
+        $new_post->fill($data);
+        $new_post->save();
+
+        return redirect()->route('admin.posts.show', $new_post);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        $categories = Category::all();
+        return view('admin.posts.show', compact('post', 'categories'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        $categories = Category::all();
+        return view('admin.posts.edit', compact('post', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(PostRequest $request, Post $post)
+    {
+        $data = $request->all();
+        $data['slug'] = Post::generateSlug($data['title']);
+
+        if($data['title'] != $post->title){
+            $post->slug = $data['slug'];
+        }
+        $post->update($data);
+
+        return redirect()->route('admin.posts.show', $post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('admin.posts.index')->with('delete_success', "Il post $post->title è stato cancellato con successo!");
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,77 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
+
+
+$().ready(function(){
+    // qui si mette tutto il codice JQuery e JS che avviene dopo il carimento del DOM
+
+    formValidator($('#postsCreateForm'));
+    formValidator($('#postsEditForm'));
+
+    function formValidator(form){
+        form.submit(function(event){
+            $('#error-title').hide();
+            $('#error-content').hide();
+            let errors = false;
+
+            if($('#title').val().length === 0){
+                $('#error-title').show('slow').text('Il titolo è un campo obbligatorio').fadeOut(5000);
+                errors = true;
+            }
+            if($('#title').val().length < 3 && $('#title').val().length != 0){
+                $('#error-title').show('slow').text('Devi inserire almeno 3 caratteri').fadeOut(5000);
+                errors = true;
+            }
+            if($('#title').val().length > 255 && $('#title').val().length != 0){
+                $('#error-title').show('slow').text('Puoi inserire massimo 255 caratteri').fadeOut(5000);
+                errors = true;
+            }
+
+
+            if($('#content').val().length === 0){
+                $('#error-content').show('slow').text('Il contenuto è un campo obbligatorio').fadeOut(5000);
+                errors = true;
+            }
+            if($('#content').val().length < 10 && $('#content').val().length != 0){
+                $('#error-content').show('slow').text('Devi inserire almeno 10 caratteri').fadeOut(5000);
+                errors = true;
+            }
+
+            if(errors){
+                event.preventDefault();
+            }
+        });
+
+    }
+
+})
